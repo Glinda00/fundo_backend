7452261b34ec7c134d5fe621cdfee883a894fb98
huguiling <hugl00@163.com>
2020-06-11 14:30:31 +0800

    init

---
+++ src/main/java/com/fundo/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.fundo.controller;
+
+import com.fundo.bean.Result;
+import com.fundo.bean.User;
+import com.fundo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 注册
+     * @param user 参数封装
+     * @return Result
+     */
+    @PostMapping(value = "/register")
+    public Result register(User user){
+        return userService.register(user);
+    }
+
+    /**
+     * 登录
+     * @param user 参数封装
+     * @return Result
+     */
+    @PostMapping(value = "/login")
+    public Result login(User user){
+        return userService.login(user);
+    }
+}
+
